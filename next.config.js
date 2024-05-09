99dcd11360fafdec8e36d8b6224d167c4c5dff1c
hoangvo <vophuochoang93@gmail.com>
2024-05-10 01:22:24 +0700

    add config to fix error download CV

--- next.config.js
+++ next.config.js
@@ -8,6 +8,19 @@ const nextConfig = {
       },
     ],
   },
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.pdf$/,
+      use: {
+        loader: 'file-loader',
+        options: {
+          name: '[path][name].[ext]',
+        },
+      },
+    });
+    return config;
+  }
 };
 
 module.exports = nextConfig;
